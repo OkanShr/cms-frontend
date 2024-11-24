98234e1bd91d8f36d602ad06732ce41766a72feb
OkanShr <okan.shr@hotmail.com>
2024-11-24 23:42:40 +0100

    Fixed Failed to load pdf Added download Increased Pdf width

--- src/components/clients/ClientPdfModal.jsx
+++ src/components/clients/ClientPdfModal.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import { Modal } from "react-bootstrap";
 import DocViewer, { DocViewerRenderers } from "@cyntler/react-doc-viewer";
 import { deleteClientPdf } from "../../api/clientApi";
@@ -6,6 +6,7 @@ import { deleteAppointmentPdf } from "../../api/appointmentApi";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import { classNames } from "@react-pdf-viewer/core";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
@@ -17,10 +18,10 @@ const ClientPdfModal = ({
   loginDetails,
   fetchClientPdfs,
 }) => {
+  const [numPages, setNumPages] = useState(null);
+
   const Hide = () => setShowPdfModal(false);
-  {
-    console.log(file);
-  }
+
   const handleDelete = () => {
     if (file.fileName.endsWith(".pdf")) {
       try {
@@ -64,12 +65,31 @@ const ClientPdfModal = ({
     return [formattedDoc];
   }
 
+  const onDocumentLoadSuccess = ({ numPages }) => {
+    setNumPages(numPages);
+  };
+
   return (
     <Modal show={showPdfModal} onHide={Hide} size="lg" centered>
-      <Modal.Body style={{ height: "80vh", overflow: "auto" }}>
+      <Modal.Body
+        className="flex"
+        style={{ height: "80vh", overflowY: "auto", padding: "0" }}
+      >
         {file.fileName.endsWith(".pdf") ? (
-          <Document file={file.filePath} options={options}>
-            <Page />
+          <Document
+            file={file.filePath}
+            options={options}
+            onLoadSuccess={onDocumentLoadSuccess}
+            className="pdf-document"
+          >
+            {Array.from(new Array(numPages), (el, index) => (
+              <Page
+                key={`page_${index + 1}`}
+                pageNumber={index + 1}
+                width={1000} // Adjusts page width dynamically.
+                className="pdf-page"
+              />
+            ))}
           </Document>
         ) : (
           <DocViewer
@@ -79,11 +99,19 @@ const ClientPdfModal = ({
         )}
       </Modal.Body>
       <Modal.Footer>
+        <a
+          href={file.filePath}
+          download={file.fileName}
+          className="custom-button"
+          style={{ marginRight: "auto" }}
+        >
+          Download
+        </a>{" "}
         <button onClick={handleDelete} className="custom-button-negative">
           Delete
         </button>
         <button className="custom-button" onClick={Hide}>
-          Schließen
+          Close
         </button>
       </Modal.Footer>
     </Modal>
